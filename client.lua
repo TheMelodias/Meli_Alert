32479590328cb1d5fbd36fe94846f587f50a2213
Melodias <158349743+TheMelodias@users.noreply.github.com>
2024-12-24 17:31:14 +0100

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,7 @@
+Citizen.CreateThread(function ()
+    while true do
+        Citizen.Wait(300000)
+        TriggerEvent('chatMessage', 'Sangre FriaRP', {255, 0, 0}, "Recuerda leerte la normativa: ")
+    end
+    
+end)
